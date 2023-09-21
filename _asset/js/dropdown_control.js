9985a1b3bb35cbb801180397137924621e3ec6b1
TheEvilApple <vagvolgyi.balazs.vb@gmail.com>
2023-09-21 18:35:14 +0200

    js befejezése

--- _asset/js/dropdown_control.js
+++ _asset/js/dropdown_control.js
@@ -1,1 +1,12 @@
 const toggleBtn = document.querySelector(".toggle_button")
+const toggleBtnIcon = document.querySelector(".toggle_button i")
+const dropDownMenu = document.querySelector(".dropdown_menu")
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle("open")
+    const isOpen = dropDownMenu.classList.contains("open")
+
+    toggleBtnIcon.classList = isOpen
+    ? "fa-solid fa-xmark"
+    : "fa-solid fa-bars"
+}
